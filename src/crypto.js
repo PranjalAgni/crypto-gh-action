fd5f14ceaead9a416a13c923098c133a24f31fa0
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-07-18 23:30:30 +0530

    :rocket: fetching live crypto prices

---
+++ src/crypto.js
@@ -0,0 +1,23 @@
+const crypto = require("crypto");
+
+const algorithm = "aes-256-ctr";
+const secretKey = "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3";
+
+const decryptAPIData = (hash) => {
+  const decipher = crypto.createDecipheriv(
+    algorithm,
+    secretKey,
+    Buffer.from(hash.iv, "hex")
+  );
+
+  const decrpyted = Buffer.concat([
+    decipher.update(Buffer.from(hash.content, "hex")),
+    decipher.final(),
+  ]);
+
+  return decrpyted.toString();
+};
+
+module.exports = {
+  decryptAPIData,
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hi, I will be built shortly...");
+const axios = require("axios");
+const { decryptAPIData } = require("./crypto");
+
+const encryptedAPIKeyData = {
+  iv: "869f0af7fb12fe62b818248b9bd79d34",
+  content: "e09f5b10c4e95193ee1efd28fdc066a911f26e1345696baf218af24a3fbaecf1",
+};
+
+const getAPIUrl = () => {
+  const BASE_API_URL = "http://api.coinlayer.com/api/live";
+  const API_KEY = decryptAPIData(encryptedAPIKeyData);
+  console.log("KEY: ", API_KEY);
+  return `${BASE_API_URL}?access_key=${API_KEY}`;
+};
+
+const getStats = async (cryptoExchangeInstance) => {
+  const API_URL = getAPIUrl();
+  const response = await axios(API_URL);
+  console.log("Live market prices: ", response.data);
+};
+
+getStats();
